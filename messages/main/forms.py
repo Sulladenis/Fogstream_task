2e7c618172a54b32393fd6abdcf5aadf0d7328df
Sulla <sulladenis@gmail.com>
2019-06-26 11:41:43 +1000

    edited forms

--- messages/main/forms.py
+++ messages/main/forms.py
@@ -1,7 +1,21 @@
+
 from django.contrib.auth.models import User
 from django import forms
 
 class UserRegForm(forms.ModelForm):
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput,
+    #                            help_text=password_validation.password_validators_help_text_html())
+    password2 = forms.CharField(label='Пароль (повторно)',
+                                widget=forms.PasswordInput,
+                                help_text='Введите пароль повторно для проверки')
+                                
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.VaidationError('Пароль не подходит')
+        return cd['password2']
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
