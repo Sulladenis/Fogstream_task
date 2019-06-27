933dc1d4c146cff2b7b5511a338832494ffba9e4
sulladenis <sulladenis@gmail.com>
2019-06-27 21:21:00 +1000

    function sends an email with message text and api data

--- messages/main/utilities.py
+++ messages/main/utilities.py
@@ -1,11 +1,42 @@
 from django.contrib.auth.models import User
+import json
+from urllib.request import urlopen
+
 
 def post_save_dispacher(sender, **kwargs):
+    """This function sends an email with message text and api data"""
     if kwargs['created']:
         admin = User.objects.get(username='admin')
         obj_msg = kwargs['instance']
         subject = obj_msg
-        body = obj_msg.text
         email = obj_msg.email
+        body = "Вам отправленно сообщение:\n" + obj_msg.text + "\n\nДанные пользователя:\n" + get_user_data(email)
         admin.email_user(subject, body)
-        print('Сообщение {} созданно'.format(kwargs['instance']))
+
+def get_user_data(email):
+    """This function receives data from api"""
+    url = "http://jsonplaceholder.typicode.com/users?email="
+    user_data = ""
+    try:
+        with urlopen(url+email) as response:
+            source = response.read()
+    except:
+        source = False
+    if source:
+        data = json.loads(source)
+        for key in data[0].keys():
+            if type(data[0][key]) == dict:
+                user_data = user_data + "{}:\n".format(key) 
+                for k in data[0][key].keys():
+                    user_data = user_data + "    {}: {} \n".format(k, data[0][key][k])            
+            else:
+                user_data = user_data + "{}: {} \n".format(key, data[0][key])
+    return user_data
+
+
+
+
+
+ 
+
+
